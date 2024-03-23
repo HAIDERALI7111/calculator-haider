b41ff9be46560655567769d29c21a4a42480e6eb
HAIDERALI7111 <159697057+HAIDERALI7111@users.noreply.github.com>
2024-03-24 03:51:50 +0500

    npx calculator

---
+++ main.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answer = await inquirer.prompt([
+    {
+        name: "firstNumber",
+        type: "number",
+        message: "Enter your first Number"
+    },
+    {
+        name: "secondNumber",
+        type: "number",
+        message: "Enter your second Number"
+    },
+    {
+        name: "operator",
+        type: "list",
+        message: "choose your operator",
+        choices: ["+", "-", "*", "/"]
+    }
+]);
+if (answer.operator === "+") {
+    console.log(chalk.green(answer.firstNumber + answer.secondNumber));
+}
+else if (answer.operator === "-") {
+    console.log(chalk.yellow(answer.firstNumber - answer.secondNumber));
+}
+else if (answer.operator === "*") {
+    console.log(chalk.magenta(answer.firstNumber * answer.secondNumber));
+}
+else if (answer.operator === "/") {
+    console.log(chalk.blue(answer.firstNumber / answer.secondNumber));
+}
